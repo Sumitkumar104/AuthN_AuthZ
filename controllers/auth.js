438d618a16d531d0ec9849d33d9ee601bd8491f3
Sumitkumar104 <parjapatsumit104@gmail.com>
2023-12-29 15:06:41 +0530

    make the signup controller

---
+++ controllers/auth.js
@@ -0,0 +1,65 @@
+const bcrypt = require("bcrypt");
+const userschema = require("../model/schema");
+
+
+// function for signup
+exports.signup = async (req, res) => {
+    try {
+
+        // fetch the data from the frontend send by the client to server.
+        const { name, password, role, email } = req.body();
+        let encryptpassword;
+
+        // check if email enter by the user is already exist or not.
+        const existinguserdata = await userschema.findOne({ email });
+
+        if (existinguserdata != null) // if existinguserdata != NULL(if found  user)
+        {
+            return res.status(500).json({
+                message: "user is already exist",
+                success: false,
+            })
+        }
+
+
+        // now try to hash to pasword using bcrypt library     
+        const saltround = 10;
+        try {
+            encryptpassword = await bcrypt.hash(password, saltround)
+        }
+        catch (err) {
+            return res.status(500).json({
+                success: false,
+                message: "there is some error in hashing the password",
+            })
+        }
+
+
+        // Now create the entry of user in our database
+        const createentry = await userschema.create({
+            name, password: encryptpassword, role, email
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "user created successfully",
+            data: createentry,
+
+        })
+
+
+
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        success:false,
+        message:"There is error in process of signup please try again later"
+      })
+    }
+
+}
+
+exports.login=async (req,res)=>{
+    
+}
--- index.js
+++ index.js
@@ -6,14 +6,15 @@ app.use(express.json());
 require("dotenv").config();
 const port = process.env.port || 4000;
 
-// const router = require("./routes/route");
-// app.use("/api/v1", router);
+const router = require("./routes/route");
+app.use("/api/v1", router);
 
 dbconnect();
 
 app.listen(port, () => {
     console.log(`your app is listening at port = ${port}`);
 });
+
 app.get("/", (req, res) => {
-    res.send("<h1>welcome to our home page</h1>");
+    res.send("<h1>welcome to our home page updated</h1>");
 });
