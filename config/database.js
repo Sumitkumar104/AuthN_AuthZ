71aa4cd45ed57492f62e522ad1db21a3b8aea8ec
Sumitkumar104 <parjapatsumit104@gmail.com>
2023-12-29 09:41:09 +0530

    Initial setup except controllers

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose =require("mongoose");
+require("dotenv").config();
+
+exports.dbconnect = ()=>{
+ 
+        mongoose.connect(process.env.mongodburl,{})
+        .then(
+           ()=>{console.log("your connection with database is successfull created")}
+        )   
+    .catch( (err)=>{
+        console.log("there is something error to  connection with database");
+        console.error(err);
+        process.exit(1);    
+    }
+    )
+}
